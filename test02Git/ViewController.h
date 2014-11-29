4e06d50548be51dbb299a29b72584cd7a7f7f418
mohammadebrahim <prosares@prosaress-Mac.local>
2014-11-29 15:22:36 +0530

    Initial Commit

---
+++ test02Git/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  test02Git
+//
+//  Created by prosares on 11/29/14.
+//  Copyright (c) 2014 prosares. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ test02GitTests/test02GitTests.m
@@ -0,0 +1,34 @@
+//
+//  test02GitTests.m
+//  test02GitTests
+//
+//  Created by prosares on 11/29/14.
+//  Copyright (c) 2014 prosares. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface test02GitTests : XCTestCase
+
+@end
+
+@implementation test02GitTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
